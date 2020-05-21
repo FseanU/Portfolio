22c0fa24070d94b2bf5edf109b8563954bf22be8
Sean Fu <sean14xx@gmail.com>
2020-05-21 15:45:21 +0200

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get 'home', to: 'pages#home'
+  root to: 'pages#home'
+  get 'about', to: 'pages#about'
+
 end
