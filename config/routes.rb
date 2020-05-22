ec3c433fedddab1fb463d0be7b83c0c81c21195d
Sean Fu <sean14xx@gmail.com>
2020-05-22 13:37:16 +0200

    add project routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'pages#home'
-  get 'about', to: 'pages#about'
+  get 'jamin', to: 'pages#jamin'
+  get 'project1', to: 'pages#project1'
 
 end
