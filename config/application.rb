dec2f9248b5b00820929f24524f37e6c530aa3f5
Sean Fu <sean14xx@gmail.com>
2020-05-31 16:18:14 +0200

    test

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,9 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+# test
+config.assets.initialize_on_precompile = false
+
 module Portfolio
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
@@ -17,3 +20,4 @@ module Portfolio
     # the framework and any gems in your application.
   end
 end
+
