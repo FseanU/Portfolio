774cfc77f9734f005651b10190c526565ffa1504
Sean Fu <sean14xx@gmail.com>
2020-06-01 19:05:20 +0200

    animate elements on jamin page

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -16,6 +16,7 @@ require("channels")
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 import anime from 'animejs/lib/anime.es.js';
+import { gsap } from 'gsap';
 import { scrollAppear, smoothScroll } from '../components/page_text'
 
 document.addEventListener('turbolinks:load', () => {
@@ -25,6 +26,12 @@ document.addEventListener('turbolinks:load', () => {
   const contactBtn = document.querySelector('.contactBtn');
   const arrowLink = document.querySelector(".arrowLink");
   const arrow = document.querySelector("#arrow");
+  const year = document.querySelector('.year');
+  const title = document.querySelector('.project-info h1');
+  const overview = document.querySelector('.overview');
+  const descr01 = document.querySelector('.descr-01');
+  
+
   // smoothScroll
   if (projectBtn) projectBtn.addEventListener('click', ()=> {
     smoothScroll('.project', 1000);
@@ -38,16 +45,6 @@ document.addEventListener('turbolinks:load', () => {
 
 
   // arrowLink animation
-  // arrowLink.addEventListener('mouseover', () => {
-  //   const timeline = anime.timeline({
-  //     duration: 500,
-  //     easing: 'cubicBezier(.5, .05, .1, .3)'
-  //   });
-  //   timeline.add({
-  //     targets: "#arrow",
-  //     rotate: 320
-  //   })
-  // })
   if (arrowLink) arrowLink.addEventListener('mouseover', () => {
     anime({
       targets: '#arrow',
@@ -61,4 +58,13 @@ document.addEventListener('turbolinks:load', () => {
     arrow.style.transform = "translateY(0)"
     anime.remove('#arrow');
   })
+
+  // jamin page animation
+  let tl = gsap.timeline();
+  if (year) tl.fromTo( ".line", {width: "0%"}, {width: "100%", duration: 1, ease: "power1.in"})
+    .fromTo(year, {opacity: "0%"}, {opacity: "100%", duration:1, ease: "power2.in"})
+    .fromTo(title, {opacity: "0%"}, {opacity: "100%", ease: "power2.in"}, "<0")
+    .fromTo(overview, {opacity: "0%"}, {opacity: "100%", ease: "power2.in"}, "<0.5")
+    .fromTo(descr01, {opacity: "0%"}, {opacity: "100%", ease: "power2.in"}, "<0.5");
+    
 })
