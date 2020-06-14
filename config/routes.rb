1e70a7e15aecc3c86a188f942301d7686bb2b7c9
Sean Fu <sean14xx@gmail.com>
2020-06-14 16:26:09 +0200

    add about route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'pages#home'
   get 'jamin', to: 'pages#jamin'
-  get 'project1', to: 'pages#project1'
+  get 'about', to: 'pages#about'
 
 end
