1ec6d730fa23891377e4a49a6a21a235325d8b2d
Sean Fu <sean14xx@gmail.com>
2020-06-15 14:46:18 +0200

    add hover function

---
+++ app/javascript/components/hover.js
@@ -0,0 +1,17 @@
+import * as THREE from 'three';
+import hoverEffect from 'hover-effect'
+import side from 'images/profile-side.png'
+import front from 'images/profile-front.png'
+import diss from 'images/diss.png'
+
+function hover (){
+  new hoverEffect({
+    parent: document.querySelector('.distortion'),
+    intensity: 0.1,
+    image1: side,
+    image2: front,
+    displacementImage: diss
+  })
+}
+
+export { hover };
